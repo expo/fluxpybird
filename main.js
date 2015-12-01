acef70feb9fa3ebfea58eb65b862d122b67b3a79
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2015-12-01 14:33:08 -0800

    basic redux example

--- main.js
+++ main.js
@@ -3,33 +3,73 @@
  */
 'use strict';
 
-let React = require('react-native');
-let {
+import React from 'react-native';
+const {
   AppRegistry,
   StyleSheet,
   Text,
   View,
 } = React;
 
-class Main extends React.Component {
-  render() {
-    return (
-      <View
-        style={styles.container}>
-        <Text>
-          HELLO WORLD!
-        </Text>
-      </View>
-    );
-  }
-}
+import { connect, Provider } from 'react-redux/native';
+import { createStore } from 'redux';
 
-let styles = StyleSheet.create({
-  container: {
+
+/**
+ * State / Reduce
+ */
+
+const start = {
+  count: 0,
+};
+
+const reduce = (state, action) => {
+  switch (action.type) {
+    case 'CLICK':
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+
+    default:
+      return state;
+  }
+};
+
+
+/**
+ * Render
+ */
+
+const Game = connect(
+  ({ count }) => ({ count })
+)(
+  ({ count, dispatch }) => (
+    <View style={styles.gameContainer}>
+      <Text onPress={() => dispatch({ type: 'CLICK' })}>
+        YOU CLICKED ME {count} TIME{count === 1 ? '' : 'S'}!
+      </Text>
+    </View>
+  )
+);
+
+const styles = StyleSheet.create({
+  gameContainer: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+
+/**
+ * Main
+ */
+
+const Main = () => (
+  <Provider store={createStore(reduce, start)}>
+    {() => <Game />}
+  </Provider>
+);
 
 AppRegistry.registerComponent('main', () => Main);
