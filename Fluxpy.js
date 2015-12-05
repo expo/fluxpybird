0ebb859ae41fb35f44e2f80e356240e82f1a7e66
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2015-12-05 00:45:08 -0800

    position containers absolutely

--- Fluxpy.js
+++ Fluxpy.js
@@ -375,6 +375,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'transparent',
+    position: 'absolute',
+    top: 0, left: 0,
+    width: SCREEN_WIDTH, height: SCREEN_HEIGHT,
   },
   scoreContainer: {
     position: 'absolute',
